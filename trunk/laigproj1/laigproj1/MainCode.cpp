942abfbdcf4b31595420246bab9a57fd3a38bd25
inpanzinator <inpanzinator@14351ad3-35c3-758e-3178-7e904a9d9d0f>
2010-09-25 16:04:06 +0000

    Alteração da função para calcular a normal do vector.

--- trunk/laigproj1/laigproj1/MainCode.cpp
+++ trunk/laigproj1/laigproj1/MainCode.cpp
@@ -233,7 +233,7 @@ void desenhaHospital(GLUquadric * quad)
 	float vQuad2[3] = {-(telhadoComp-telhadoAresta)/2-telhadoComp/2, telhadoAlt, -telhadoLarg/2};
 	float vQuadNor[3] = {vQuad1[1]*vQuad2[2]-vQuad1[2]*vQuad2[1], vQuad1[2]*vQuad2[0]-vQuad1[0]*vQuad2[2], vQuad1[0]*vQuad2[1]-vQuad1[1]*vQuad2[0]}; 
 	float normaVecQuad = sqrt(vQuadNor[0]*vQuadNor[0]+vQuadNor[1]*vQuadNor[1]+vQuadNor[2]*vQuadNor[2]);
-	for(i=0; i < 3; i++) vQuadNor[i] = vQuadNor[i]/normaVec;
+	for(i=0; i < 3; i++) vQuadNor[i] = vQuadNor[i]/normaVecQuad;
 
 	glPushMatrix();
 	glTranslatef((PosChaoCX1+PosChaoCX2)/2,hospAlt,3*(PosChaoZ2/4)+telhadoLarg/2);
